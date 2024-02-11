8f33c6843b5c7b1da21582c52c37a5ad59dd650d
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-02-11 22:03:54 +0200

    make build

---
+++ index.9ed079a3.js
@@ -0,0 +1,3 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");e.src=r.href});//# sourceMappingURL=index.9ed079a3.js.map
+
+//# sourceMappingURL=index.9ed079a3.js.map
